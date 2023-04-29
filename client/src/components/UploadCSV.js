2ccb9a05e04632479fcd362f8c9d47498b8ed417
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2023-04-29 18:47:56 +0530

    app deployed , final changes with ui implemented,

--- client/src/components/UploadCSV.js
+++ client/src/components/UploadCSV.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
-import BubblePlot from "./BubblePlot";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import BubblePlot from "./BubblePlot";
 
 export default function UploadCSV() {
   const [file, setFile] = useState();
   const [array, setArray] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
 
   const fileReader = new FileReader();
 
@@ -27,16 +32,36 @@ export default function UploadCSV() {
     setArray(array);
   };
 
-  const handleOnSubmit = (e) => {
+  const handleOnSubmit = async (e) => {
     e.preventDefault();
 
     if (file) {
-      fileReader.onload = function (event) {
-        const text = event.target.result;
-        csvFileToArray(text);
-      };
+      setLoading(true);
+      setSuccess(false);
+      setError(false);
 
-      fileReader.readAsText(file);
+      try {
+        const formData = new FormData();
+        formData.append("csv_file", file);
+
+        const response = await fetch("http://127.0.0.1:5000/upload", {
+          method: "POST",
+          body: formData,
+        });
+
+        if (response.ok) {
+          const text = await response.text();
+          csvFileToArray(text);
+          setSuccess(true);
+        } else {
+          throw new Error("Network response was not ok.");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        setError(true);
+      }
+
+      setLoading(false);
     }
   };
 
@@ -51,40 +76,44 @@ export default function UploadCSV() {
           id={"csvFileInput"}
           accept={".csv"}
           onChange={handleOnChange}
+          style={{ marginRight: "10px" }}
         />
 
+        <br />
         <button
+          style={{
+            backgroundColor: "#333",
+            color: "#eee",
+            padding: "12px 24px",
+            border: "none",
+            borderRadius: "4px",
+            cursor: "pointer",
+            transition: "background-color 0.3s ease",
+            marginRight: "10px",
+            marginBottom: "10px",
+          }}
           onClick={(e) => {
             handleOnSubmit(e);
           }}
         >
-          IMPORT CSV
+          Send
         </button>
       </form>
 
-      <br />
+      {loading && (
+        <div>
+          <p>Uploading file and generating plots... <FontAwesomeIcon icon={faSpinner} spin /></p>
+        </div>
+      )}
 
-      {/* <table>
-        <thead>
-          <tr key={"header"}>
-            {headerKeys.map((key) => (
-              <th>{key}</th>
-            ))}
-          </tr>
-        </thead>
+      {success && (
+        <div>
+          <p>Generated plots successfully!</p>
 
-        <tbody>
-          {array.map((item) => (
-            <tr key={item.id}>
-              {Object.values(item).map((val) => (
-                <td>{val}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table> */}
+        </div>
+      )}
 
-      {array.length > 0 && <BubblePlot data={array} />}
+      {error && <p>Error generating plots. Please try again.</p>}
     </div>
   );
 }
