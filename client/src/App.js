6c61f7644d939fc34c878b6d2fc4c04056c2fc7e
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2023-04-29 18:47:56 +0530

    app deployed , final changes with ui implemented,

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 import GetPlot from './components/GetPlot';
+import UploadCSV from './components/UploadCSV';
 
 function App() {
   return (
     <div className="App">
+      <UploadCSV/>
       <GetPlot />
     </div>
   );
